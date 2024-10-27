b3300a975466aad0e5335d9c05715fcaf872bce4
arobaseSuulei <souleymanediallo2004@outlook.fr>
2024-10-27 16:46:01 +0100

    update

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Home from "./Home";
 import Dashboard from "./components/Dashbord";
 import './App.css'
@@ -17,44 +17,55 @@ import Footer from "./components/Footer";
 
 export default function App() {
 
-  return(
+    useEffect(() => {
+        document.title = "FinanceMate | mate of personal finance";
+    }, []);
+  return (
+
       <div>
-          <div className={'grid grid-cols-3 gap-2'}>
-              <nav style={{backgroundColor: '#111313'}} className={' col-span-3 rounded-lg p-4  mx-4 my-4   '}>
-                  <Navigation/>
-              </nav>
 
-              <nav style={{backgroundColor: '#111313'}} className={' row-span-3 rounded-lg  mx-4 my-4   '}>
-                  <Dashboard/>
-              </nav>
+          <h1 className={'block sm:hidden text-white p-4'}>We recommend you to use FinanceMate on your Desktop</h1>
+          <div className={'hidden sm:block'}>
 
+              <div className={'grid grid-cols-3 gap-2'}>
+                  <nav style={{backgroundColor: '#111313'}} className={' col-span-3 rounded-lg p-4  mx-4 my-4   '}>
 
-              <nav style={{backgroundColor: '#C4E8D1'}} className={'mx-4 my-4 rounded-lg '}>
-                  <Revenu/>
-              </nav>
-              <nav style={{backgroundColor: '#C4E8D1'}} className={'mx-4 my-4 rounded-lg'}>
-                  <Depense/>
-              </nav>
+                      <Navigation/>
+                  </nav>
+
+                  <nav style={{backgroundColor: '#111313'}} className={' row-span-3 rounded-lg  mx-4 my-4   '}>
+                      <Dashboard/>
+                  </nav>
 
 
-              <nav className={'mx-4 my-4 border rounded-lg bg-white px-8'}>
-                  <OperationRev/>
-              </nav>
-              <nav className={'mx-4 my-4 border rounded-lg bg-white px-8'}>
-                  <OperationDep/>
-              </nav>
-              <nav className={' border rounded-lg bg-white px-8 mx-4 my-4'}>
-                  <OperationDep/>
-              </nav>
+                  <nav style={{backgroundColor: '#C4E8D1'}} className={'mx-4 my-4 rounded-lg '}>
+                      <Revenu/>
+                  </nav>
+                  <nav style={{backgroundColor: '#C4E8D1'}} className={'mx-4 my-4 rounded-lg'}>
+                      <Depense/>
+                  </nav>
 
-              <nav style={{backgroundColor: '#111313'}} className={'text-white p-4  col-span-3 rounded-lg bg-white px-8 mx-4 my-4'}>
-                  <Footer/>
-              </nav>
+
+                  <nav className={'mx-4 my-4 border rounded-lg bg-white px-8'}>
+                      <OperationRev/>
+                  </nav>
+                  <nav className={'mx-4 my-4 border rounded-lg bg-white px-8'}>
+                      <OperationDep/>
+                  </nav>
+                  <nav className={' border rounded-lg bg-white px-8 mx-4 my-4'}>
+                      <OperationDep/>
+                  </nav>
+
+                  <nav style={{backgroundColor: '#111313'}}
+                       className={'text-white p-4  col-span-3 rounded-lg bg-white px-8 mx-4 my-4'}>
+                      <Footer/>
+                  </nav>
+
+
+              </div>
 
 
           </div>
-
-
       </div>
 
 
--- my-project/src/components/Footer.jsx
+++ my-project/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function Footer() {
     return(
         <div className={'flex text-xs justify-center items-center gap-4'}>
-            <p>made by souleymane</p>
+            <p>made by <a className={'underline'} target={'_blank'} href={'https://souleyd-portfolio.vercel.app'}>souleymane</a></p>
             <p>|</p>
             <p className={''}>database hosted by supabase</p>
             <img className={'w-6 rounded-full'} src="https://img.logo.dev/supabase.com?token=pk_QyZYYVI6QBejCt9wU_0qdw"/>
